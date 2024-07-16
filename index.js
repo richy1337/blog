fc218425087ab65a7746e64d54b9d2ef9e82dbf5
Richard <richardhan0530@gmail.com>
2024-07-15 21:41:48 -0400

    added article adding page

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
     res.render("index.ejs");
 });
 
+app.get('/form', (req, res) => {
+    res.render("form.ejs");
+});
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
