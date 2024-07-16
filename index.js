525e080cd35b07f939aa0b5b5843b729a93f33bf
richardhan0530@gmail.com <richardhan0530@gmail.com>
2024-07-16 02:07:07 -0400

    new post function

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 import express from "express";
+import bodyParser from "body-parser"
 
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let blogPosts = [];
 
 app.get('/', (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", { blogPosts: blogPosts });
 });
 
 app.get('/form', (req, res) => {
     res.render("form.ejs");
+});
+
+app.post('/submit-article', (req, res) => {
+    const title = req.body.title;
+    const content = req.body.content;
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    today = mm + '/' + dd + '/' + yyyy;
+
+    const newPost = { title: title, content: content, datePosted: today };
+    blogPosts.push(newPost);
+
+    res.redirect('/');
 });
 
 app.listen(port, () => {
